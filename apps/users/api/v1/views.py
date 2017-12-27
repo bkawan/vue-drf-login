ceeddcc4fd61b111a273ae0d62814466b7d63401
bkawan <bikeshkawang@gmail.com>
2017-12-28 00:34:11 +0545

    modify get_object and get_queryset

--- apps/users/api/v1/views.py
+++ apps/users/api/v1/views.py
@@ -9,3 +9,15 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     parser_classes = (MultiPartParser, FormParser, JSONParser)
+
+    def get_queryset(self, **kwargs):
+        qs = self.queryset
+        if self.kwargs.get('pk') == 'me':
+            return qs.filter(id=self.request.user.id)
+        return qs
+
+    def get_object(self):
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        if self.kwargs[lookup_url_kwarg] == 'me':
+            return self.get_queryset().get()
+        return super().get_object()
