60d218a52f59b4c240244bdc64ac9cd4ef4646c5
bkawan <bikeshkawang@gmail.com>
2018-01-29 13:04:15 +0545

    api end point for singup

--- apps/users/api/v1/urls.py
+++ apps/users/api/v1/urls.py
@@ -1,11 +1,12 @@
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import UserViewSet, SignupView
 
 app_name = 'users_api_v1'
+urlpatterns = [
+    url(r'^signup/$', SignupView.as_view())
+]
 router = DefaultRouter()
 router.register('users', UserViewSet)
-urlpatterns = router.urls
-
-
-
+urlpatterns += router.urls
